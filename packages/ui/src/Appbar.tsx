0ce32460b927159d918c484bc87ed209a45b3d80
saptarshi <saptarshimajumder12345@gmail.com>
2024-04-11 12:17:48 +0530

    commit

--- packages/ui/src/Appbar.tsx
+++ packages/ui/src/Appbar.tsx
@@ -15,8 +15,8 @@ export const Appbar = ({
     onSignout
 }: AppbarProps) => {
     return <div className="flex justify-between border-b px-4 border-slate-300">
-        <div className="text-lg flex flex-col justify-center">
-            PayTM
+        <div className="text-lg font-bold flex flex-col justify-center">
+            CashFlow
         </div>
         <div className="flex flex-col justify-center pt-2">
             <Button onClick={user ? onSignout : onSignin}>{user ? "Logout" : "Login"}</Button>
