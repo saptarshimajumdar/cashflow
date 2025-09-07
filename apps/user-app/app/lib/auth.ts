1fba64b4e5d165c11865647b8413317f08419b08
Saptarshi Majumdar <64518129+saptarshimajumdar@users.noreply.github.com>
2025-09-07 12:56:51 +0530

    Update auth.ts

--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -12,7 +12,6 @@ export const authOptions = {
           },
           
           async authorize(credentials: any) {
-            // Do zod validation, OTP validation here
             const hashedPassword = await bcrypt.hash(credentials.password, 10);
             const existingUser = await db.user.findFirst({
                 where: {
